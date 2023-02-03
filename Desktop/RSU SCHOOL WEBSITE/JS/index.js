af0252537e088b68d012fc25380854c39af876c3
kalebgods <kalebgods@gmail.com>
2023-02-03 22:01:18 +0100

    initial commit

---
+++ Desktop/RSU SCHOOL WEBSITE/JS/index.js
@@ -0,0 +1,13 @@
+function showMessage(text) {
+    
+    
+    if (text === undefined) {
+        text = 'empty message';
+    } else {
+        text = 'a test';
+    }
+    alert(text);
+    
+}
+// ignore this line of code cos it is a comment
+showMessage('boy');
